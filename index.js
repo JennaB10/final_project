1536129508af9de0207a54f0a7d1ace4c78f09ef
Jenna <jennb1046@gmail.com>
2021-03-07 19:17:20 -0600

    sunday

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 firebase.auth().onAuthStateChanged(async function(user) {
+
+  let db = firebase.firestore()
+
   if (user) {
     // Signed in
     console.log('signed in')
+
+    
+    db.collection('users').doc(user.uid).set({ 
+      name: user.displayName,
+      email: user.email
+    })
+
+        // Create a sign-out button
+        document.querySelector('.sign-in-or-sign-out').innerHTML = `
+        <button class="text-pink-500 underline sign-out">Sign Out</button>
+      `
+        document.querySelector('.sign-out').addEventListener('click', function(event) {
+        console.log('sign out clicked')
+        firebase.auth().signOut()
+        document.location.href = 'index.html'
+      })
+
+      // listen for the form submit and create new post
+
+    document.querySelector('form').addEventListener('submit', async function(event) {
+      event.preventDefault()
+      let currenteventsText = document.querySelector('#currentevents').value
+      let docRef = await db.collection('currentevents').add({
+          text: currenteventsText
+      })
+      let currenteventsId = docRef.id
+      console.log(`New Icebreaker question with ID ${currenteventsId} created`)
+      renderPost(currenteventsText)
+    })
+        
+    //Render all questions when the page is loaded
+    let querySnapshot = await db.collection('currentevents').get()
+    let currentevents = querySnapshot.docs
+    for (let i=0; i<currentevents.length; i++) {
+      // let currenteventsId = currentevents[i].id
+      // let currenteventsData = currentevents[i].data()
+       let currenteventsText = currentevents.text
+       renderPost(currenteventsText)
+    }
+   
   } else {
     // Signed out
     console.log('signed out')
@@ -21,3 +64,11 @@ firebase.auth().onAuthStateChanged(async function(user) {
     ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 })
+
+async function renderPost(currenteventsText) {
+  document.querySelector('.currentevents').insertAdjacentHTML('beforeend', `
+    <div class="post-${currenteventsText} md:mt-16 mt-8 space-y-8">
+ 
+    `)
+ 
+}
--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyBmG3BrSV-QMRkaAbkNsIys3EU26_F_18k",
+  authDomain: "kiei-451-80d14.firebaseapp.com",
+  projectId: "kiei-451-80d14",
+  storageBucket: "kiei-451-80d14.appspot.com",
+  messagingSenderId: "738741945431",
+  appId: "1:738741945431:web:19242bc6e91daeaf6b0f12"
+} 
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
